7e650a8e9590929412062e5be5076a9a20a3e1b4
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2024-03-03 02:13:13 +0600

    configured commonly used color

--- scripts/tailwind-theme-config.js
+++ scripts/tailwind-theme-config.js
@@ -2,8 +2,11 @@ tailwind.config = {
   theme: {
     extend: {
       colors: {
-        'c-primary': '#100f0f',
+        'c-primary': '#12132d',
         'c-secondary': '#706f6f',
+        'color-bg':'#12132d08',
+        'color-border':'#0307121a',
+        'c-button':'#797dfc',
       }
     }
   }
